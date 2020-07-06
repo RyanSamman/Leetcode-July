18e0abcf777a68fc6a1171b6f771a9731d83f1ba
RyanSamman <RyanSamman@outlook.com>
2020-07-06 23:05:19 +0300

    Day 5

---
+++ day 5/test.js
@@ -0,0 +1,8 @@
+let i1 = 7
+let i2 = 2
+
+let result = i1 ^ i2
+let binResult = result.toString(2)
+
+console.log(i1.toString(2), i2.toString(2))
+console.log(result, binResult)
---
+++ day 5/test.py
@@ -0,0 +1,7 @@
+i1 = 7
+i2 = 1
+
+result = i1 ^ i2
+resultBinary = "{0:b}".format(result)
+print(resultBinary.count("1"))
+print("")
